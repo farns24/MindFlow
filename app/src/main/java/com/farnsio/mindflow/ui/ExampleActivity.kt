03a054fb8150326e49a9252e5878ede6616a768b
Mike <mafarnsworth00@gmail.com>
2021-04-02 22:46:50 -0600

    initial commit

---
+++ app/src/main/java/com/farnsio/mindflow/ui/ExampleActivity.kt
@@ -0,0 +1,13 @@
+package com.farnsio.mindflow.ui
+
+import android.app.Activity
+import android.os.Bundle
+import com.farnsio.mindflow.R
+
+class ExampleActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.appwidget_provider_layout)
+    }
+}
---
+++ app/src/main/java/com/farnsio/mindflow/ui/slideshow/SlideshowViewModel.kt
@@ -0,0 +1,13 @@
+package com.farnsio.mindflow.ui.slideshow
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class SlideshowViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is slideshow Fragment"
+    }
+    val text: LiveData<String> = _text
+}
