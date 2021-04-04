e81f179afe8fa05a8697fc7ac5673583d466d856
farns24 <mafarnsworth00@gmail.com>
2021-04-04 09:12:19 -0600

    Add onclick listener to widget

--- app/src/main/java/com/farnsio/mindflow/ui/widget/NewAppWidget.kt
+++ app/src/main/java/com/farnsio/mindflow/ui/widget/NewAppWidget.kt
@@ -1,9 +1,12 @@
 package com.farnsio.mindflow.ui.widget
 
+import android.app.PendingIntent
 import android.appwidget.AppWidgetManager
 import android.appwidget.AppWidgetProvider
 import android.content.Context
+import android.content.Intent
 import android.widget.RemoteViews
+import com.farnsio.mindflow.MainActivity
 import com.farnsio.mindflow.R
 
 // Implementation of App Widget functionality.
@@ -37,10 +40,15 @@ internal fun updateAppWidget(
         appWidgetManager: AppWidgetManager,
         appWidgetId: Int
 ) {
-    val widgetText = context.getString(R.string.app_name)
     // Construct the RemoteViews object
-    val views = RemoteViews(context.packageName, R.layout.new_app_widget)
-    views.setTextViewText(R.id.appwidget_text, widgetText)
+    val pendingIntent: PendingIntent = Intent(context, MainActivity::class.java)
+        .let { intent ->
+            PendingIntent.getActivity(context, 0, intent, 0)
+        }
+
+    val views = RemoteViews(context.packageName, R.layout.new_app_widget).apply {
+        setOnClickPendingIntent(R.id.edit_button, pendingIntent)
+    }
 
     // Instruct the widget manager to update the widget
     appWidgetManager.updateAppWidget(appWidgetId, views)
